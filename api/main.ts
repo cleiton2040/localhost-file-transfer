5989894128d6e28bd7061115c85bdefe215d4ee7
cleiton2040 <grimmyshad@gmail.com>
2022-06-11 23:40:33 -0300

    tirar uns @ts-ginore

--- api/main.ts
+++ api/main.ts
@@ -10,11 +10,10 @@ const port = 8080;
 
 app.use('/public', express.static('public'));
 app.use(express.json());
-app.use(cookieParser("secret")); // @ts-ignore
-app.use(login);
+app.use(cookieParser("secret"));
+app.use('*', login as any);
 
-// @ts-ignore
-for (const x of routes) app[x.method as 'get' || 'get'](x.route, x.fn);
+for (const x of routes) app[x.method as 'get' || 'get'](x.route, x.fn as any);
 
 app.listen(8080, () => {
     console.log(`\x1b[32mListening in http://${ip.local}:${port}\x1b[0m`);
